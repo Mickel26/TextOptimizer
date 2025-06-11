6e3d3eb16ed6027e9e2d81353adebceee2272bf0
Mickel26 <mikolajfilka@gmail.com>
2025-06-11 17:43:28 +0200

    created optimizeText func

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+  const [optimizedText, setOptimizedText] = useState("");
+
+  const optimizeText = () => {
+    setOptimizedText(text);
+  };
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="flex flex-col items-center min-h-screen bg-gray-100">
+        <h1 className="text-5xl font-bold mt-14">Text Optimizer</h1>
+        <textarea
+          className="mt-14 w-1/2 p-4 bg-white rounded-lg shadow text-gray-800 border focus:outline-none focus:ring-2 focus:ring-blue-500 resize-y min-h-[3rem]"
+          name="textInput"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          rows={9}
+        />
+        <button
+          className="cursor-pointer mt-6 px-8 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+          onClick={optimizeText}
+        >
+          Optimize
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {optimizedText && (
+          <div className="mt-8 w-1/2 p-4 bg-white rounded-lg shadow text-gray-800 whitespace-pre-wrap">
+            {optimizedText}
+          </div>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
